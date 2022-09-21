d27e29945faadbbf5bfdff36b7c2ac6c72adec5a
Charlotte Curtis <ccurtis@mtroyal.ca>
2022-09-21 13:15:30 -0600

    Adding lecture04 sample code

---
+++ lecture04/hello.py
@@ -0,0 +1,23 @@
+# print("Hello, World!")
+# print("Another print statement")
+
+# # slide 6
+# print("26" * 12)
+# print(26 * 12)
+# print("Hello, world!" * 26)
+
+# slide 10
+print(0.1 + 0.1 + 0.1)
+max_float = 1.79e308
+print("max_float:", max_float)
+print("max_float * 2:", max_float * 2)
+
+# slide 11
+big_int = 10 ** 308
+print("big_int:", big_int)
+print("big_int * 2:", big_int * 2)
+
+# slide 16
+print("Standard division:", 5 / 2)
+print("Integer division:", 5 // 2)
+print("Modulo:", 5 % 2)
