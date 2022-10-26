3ec8c26205e7baf7ccce9692bade4699db9d4405
Charlotte Curtis <ccurtis@mtroyal.ca>
2022-10-26 12:38:30 -0600

    Adding incan gold example

--- lecture12/incan_gold_starter.py
+++ lecture12/incan_gold_starter.py
@@ -5,14 +5,28 @@ def draw_card() -> int:
     Draw a random number of gems between 0 and 15.
     25/75 chance of 0 or gems.
     """
+    gems_or_zero = randint(0,3)
+    n_gems = 0
+    if gems_or_zero == 0:
+        n_gems = 0
+    else:
+        n_gems = randint(1, 15)
 
+    return n_gems
 
-    return
-
-def play_game(             ) -> None:
+def play_game(n_players: int) -> None:
     """
-    
+    Draw a card. If not zero, divide gems evenly by players
+    and calculate leftover. Otherwise, lose.
     """
+    gems = draw_card()
+    print(f"You drew {gems} gems")
+    if gems == 0:
+        print("Sorry, you lose!")
+    else:
+        n_per_player = gems // n_players
+        leftover = gems % n_players
+        print(f"Everyone gets {n_per_player} gems with {leftover} leftover.")
 
 
 def main() -> None:
